2d0716c5e8cd21473f5b95529b00339ab87844c4
oeileyo <44947315+oeileyo@users.noreply.github.com>
2021-03-15 15:06:13 +0300

    Add files via upload

---
+++ spr/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+    INSERT INTO person(first_name, last_name, city, street)
+VALUES ('n1', 'f1', 'msk', 'zzz'),
+('n2', 'f2', 'msk2', 'zzz2'),
+('n3', 'f3', 'msk3', 'zzz3'),
+('n4', 'f4', 'msk4', 'zzz4');
